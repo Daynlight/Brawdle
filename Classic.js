b7137aeda1caa3921495720e5adbd039ca1433d1
Kokomichrzan <kokomichrzanuwu@gmail.com>
2024-01-08 21:03:34 +0100

    Code Cleaning

--- Classic.js
+++ Classic.js
@@ -1,102 +1,101 @@
-//Import and Varibles
-import Champions from "./Assets/Champions.json" assert {type: 'json'};
-var ChampionsTable = document.getElementById("Champions");
-var WinDiv = document.getElementById("WinChempion");
+/////// Init Program ////////////////////////////////////////////////////////
+//Import Data
+import ChampionsList from "./Assets/Champions.json" assert {type: 'json'};
+//Div Refs
+var ChampionsListDiv = document.getElementById("Champions");
+var WinnerDiv = document.getElementById("WinChempion");
 var AttribDiv = document.getElementById("ChampionsAttrib");
-var ChampionsCount = Object.keys(Champions).length;
-var Winned = false;
-var WinnerChempion = Math.floor(Math.random() * ChampionsCount);
-var AnimationTime = 300;
-var Try = 0;
+//Global Varibles
+var GameIsFinishedOrChecking = false;
+var AnimationDuration = 300;
+var TryCount = 0;
+//Get Winner
+var WinnerChempionID = Math.floor(Math.random() * Object.keys(Champions).length);
 
-//Create Table Champions
-for(var id = 0; id < ChampionsCount; id++){
-    ChampionsTable.innerHTML += 
-    `<div id="Chempion${id}">
-    <img src="Assets/Champions/${Champions[id].Icon}">
+///////// Render ChempionList and Add OnClick Event //////////////////////////
+for(var ID = 0; ID < Object.keys(ChampionsList).length; ID++){
+    ChampionsListDiv.innerHTML += 
+    `<div id="Chempion${ID}">
+    <img src="Assets/Champions/${ChampionsList[ID].Icon}">
     </div>`;
 
-    setTimeout(AddEventClick, 100, id);
+    setTimeout(AddEventClick, 100, ID);
+}
+function AddEventClick(ID){
+    document.querySelector(`#Chempion${ID}`).addEventListener('click', CheckIfChempionIsWinnerChempion);
+    document.querySelector(`#Chempion${ID}`).myParam = ID;
 }
 
-function AddEventClick(id){
-    document.querySelector(`#Chempion${id}`).addEventListener('click', CheckChempion);
-    document.querySelector(`#Chempion${id}`).myParam = id;
-}
-
-//Check Chempion
-async function CheckChempion(id){
-    var ID = id.currentTarget.myParam;
+///////// Check Chempion /////////////////////////////////////////////////////
+async function CheckIfChempionIsWinnerChempion(ElementPointer){
+    var ID = ElementPointer.currentTarget.myParam;
     var CurrentChempion = document.getElementById(`Chempion${ID}`);
 
-    if(CurrentChempion.className == "ChempionUsed" || Winned) return 0;
-    Winned = true
-    Try++;
+    if(CurrentChempion.className == "ChempionUsed" || GameIsFinishedOrChecking) return 0;
+    GameIsFinishedOrChecking = true
+    TryCount++;
     
-    CheckAttrib(ID);
+    CompareAttribs(ID);
+    await delay(AnimationDuration * 5);
 
-    await delay(AnimationTime * 5);
-
-    if(WinnerChempion == ID)
+    if(WinnerChempionID == ID)
     {
-        Winned = true;
-        WinDiv.innerHTML = `Congrats, You guessed at ${Try} time. That was ${Champions[ID].Name} <3</br>
-        <img src="Assets/Champions/${Champions[ID].Icon}">`;
+        WinnerDiv.innerHTML = `Congrats, You guessed at ${TryCount} time. That was ${ChampionsList[ID].Name} <3</br>
+        <img src="Assets/Champions/${ChampionsList[ID].Icon}">`;
         return 0;
     }
     else
     {
-        Winned = false;
+        GameIsFinishedOrChecking = false;
         CurrentChempion.className = "ChempionUsed";
         return 0;
     }
 }
+async function CompareAttribs(ID){
+    //Add Chempion to Attrib List
+    var Row = AttribDiv.getElementsByTagName('tbody')[0].insertRow(0);
 
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-  }
-
-async function CheckAttrib(ID){
-    //Add Chempion
-    var Ref = AttribDiv.getElementsByTagName('tbody')[0];
-    var Row = Ref.insertRow(0);
-    Row.insertCell(0).innerHTML = `<img class="TableIcon" src="Assets/Champions/${Champions[ID].Icon}">`;
+    //Fill Chempions Info Data
+    var Icon = Row.insertCell(0);
     var Gender = Row.insertCell(1)
     var Role = Row.insertCell(2);
     var Rarity = Row.insertCell(3);
     var Release = Row.insertCell(4);
     var Family = Row.insertCell(5);
+    Icon.innerHTML = `<img class="TableIcon" src="Assets/Champions/${ChampionsList[ID].Icon}">`;
+    Gender.innerHTML = ChampionsList[ID].Gender
+    Role.innerHTML = ChampionsList[ID].Role
+    Rarity.innerHTML = ChampionsList[ID].Rarity
+    Release.innerHTML = ChampionsList[ID].ReleaseYear
+    Family.innerHTML = ChampionsList[ID].Family
 
-    Gender.innerHTML = Champions[ID].Gender
-    Role.innerHTML = Champions[ID].Role
-    Rarity.innerHTML = Champions[ID].Rarity
-    Release.innerHTML = Champions[ID].ReleaseYear
-    Family.innerHTML = Champions[ID].Family
-
-    //Set Colors
-    await delay(AnimationTime);
-    if(Champions[ID].Gender == Champions[WinnerChempion].Gender) Gender.className = "Correct";
+    //Set Colors and Clasess for Compared Values
+    await delay(AnimationDuration);
+    if(ChampionsList[ID].Gender == ChampionsList[WinnerChempionID].Gender) Gender.className = "Correct";
     else  Gender.className = "InCorrect";
-    await delay(AnimationTime);
-    if(Champions[ID].Role == Champions[WinnerChempion].Role) Role.className = "Correct";
+    await delay(AnimationDuration);
+    if(ChampionsList[ID].Role == ChampionsList[WinnerChempionID].Role) Role.className = "Correct";
     else  Role.className = "InCorrect";
-    await delay(AnimationTime);
-    if(Champions[ID].Rarity == Champions[WinnerChempion].Rarity) Rarity.className = "Correct";
+    await delay(AnimationDuration);
+    if(ChampionsList[ID].Rarity == ChampionsList[WinnerChempionID].Rarity) Rarity.className = "Correct";
     else  Rarity.className = "InCorrect";
-    await delay(AnimationTime);
-    if(Champions[ID].ReleaseYear == Champions[WinnerChempion].ReleaseYear) Release.className = "Correct";
+    await delay(AnimationDuration);
+    if(ChampionsList[ID].ReleaseYear == ChampionsList[WinnerChempionID].ReleaseYear) Release.className = "Correct";
     else  Release.className = "InCorrect";
-    await delay(AnimationTime);
-    if(Champions[ID].Family == Champions[WinnerChempion].Family) Family.className = "Correct";
+    await delay(AnimationDuration);
+    if(ChampionsList[ID].Family == ChampionsList[WinnerChempionID].Family) Family.className = "Correct";
     else  Family.className = "InCorrect";
-
 }
 
-function AtrribFillSize(){
+//No Game Functions
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+function AtrribSetSize(){
     var WinChampionsize = document.getElementById("WinChempion").clientHeight;
     var ChampionsSize = document.getElementById("Champions").clientHeight;
     var Height = window.innerHeight - (WinChampionsize + ChampionsSize) - 30;
     if(document.getElementById("Attrib").clientHeight >= Height)document.getElementById("Attrib").style.height = `${Height}px`;
 }
 
-setInterval(AtrribFillSize, 1);
+setInterval(AtrribSetSize, 100);
